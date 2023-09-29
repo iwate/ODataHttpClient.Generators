1b5f9c5d8ad4311fd31bdbae86af6dada2e30647
iwate <github@iwate.me>
2023-09-29 18:53:59 +0900

    remove unused using

--- src/ODataHttpClient.Generators.Tests/EdmTest.cs
+++ src/ODataHttpClient.Generators.Tests/EdmTest.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Security.Cryptography.X509Certificates;
 using Xunit;
 
 namespace ODataHttpClient.Generators.Tests
